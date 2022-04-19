72ac54d50ee700e3dfbf1fc7f480c14e380bd614
awmaker <awmaker@kakao.com>
2022-04-20 03:53:26 +0900

    BUG: fix port in userdata page

--- src/pages/user/[id].js
+++ src/pages/user/[id].js
@@ -13,58 +13,53 @@ export default function Profile({ id }) {
     const [user, setUser] = useState({})
     useEffect(() => {
         alert('>>' + id)
-        axios.get(`http://localhost:5050/api/user/profile/${id}`).then(res => {
-            setUser(res.data.user)
-        }).catch(err => {
-            console.log(err)
-        })
+        axios
+            .get(`http://localhost:5000/api/user/profile/${id}`)
+            .then((res) => {
+                setUser(res.data.user)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }, [])
 
-    return (<>
-        <h1>
-            사용자 프로필
-        </h1>
-        <div>
-            <label>
-                <b>
-                    사용자 ID
-                </b>
-            </label>
+    return (
+        <>
+            <h1>사용자 프로필</h1>
+            <div>
+                <label>
+                    <b>사용자 ID</b>
+                </label>
 
-            <input type="text" name='username' value={router.query.id} />
+                <input type='text' name='username' value={router.query.id} />
 
-            <br />
+                <br />
 
-            <label htmlFor="">
-                <b>
-                    비밀번호
-                </b>
-            </label>
+                <label htmlFor=''>
+                    <b>비밀번호</b>
+                </label>
 
-            <input type="text" name='password' value={user.password} />
-            <br />
+                <input type='text' name='password' value={user.password} />
+                <br />
 
-            <label>
-                <b>
-                    이름
-                </b>
-            </label>
+                <label>
+                    <b>이름</b>
+                </label>
 
-            <input type="text" name='name' value={user.name} />
+                <input type='text' name='name' value={user.name} />
 
-            <br />
+                <br />
 
-            <label>
-                <b>
-                    전화번호
-                </b>
-            </label>
+                <label>
+                    <b>전화번호</b>
+                </label>
 
-            <input type="text" name='telephone' value={user.telephone} />
+                <input type='text' name='telephone' value={user.telephone} />
 
-            <br />
+                <br />
 
-            <br />
-        </div>
-    </>)
+                <br />
+            </div>
+        </>
+    )
 }
